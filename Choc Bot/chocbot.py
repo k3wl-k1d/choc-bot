b163c1e0132a7b9f97b32814ebf3f56235afbff7
FletchHead <ernest.li2006@gmail.com>
2025-10-19 18:56:29 -0400

    Updated battlefactory to work now

--- Choc Bot/chocbot.py
+++ Choc Bot/chocbot.py
@@ -1,5 +1,4 @@
 import discord
-from discord.ext import commands
 import random
 import os
 import string
@@ -14,7 +13,7 @@ KEY = "CHOC_DISCORD_BOT_KEY"
 TOKEN = os.getenv(KEY)
 
 # Dev script
-DEV = "fletchhead"
+DEV = 513462153093840896
 
 # Get a list of all Pokemon names
 allPokemonNames = p_data_parser.parse_names(p_data_parser.file, p_data_parser.numPokemon)
@@ -64,10 +63,11 @@ async def on_message(message):
             msgSent = True
 
             if msgContent == "!help":
-                print(f"Current commands:\n-**!stat**: Quick stat quiz\n-**!stats** or **!s**: Multi-round stats quiz\n"
+                await message.channel.send(f"Current commands:\n-**!stat**: Quick stat quiz\n-**!stats** or **!s**: Multi-round stats quiz\n"
                     "-**!battlefactory** @opp: Sends you and your opponent a BattleFactory challenge\n"
                     "-**!battlefactoryf** [League], [0, 1, or 2 (ALL, SINGLES, SPECIAL formats)], @opp: Sends you and your opponent a BattleFactory challenge based on the filters\n"
                     "-**!stop**: Stops the current action if busy")
+                
 
             # Display a quick stat quiz
             if msgContent == "!stat":
@@ -121,89 +121,94 @@ async def on_message(message):
                         failedAttempts = 0
                         currentQuestion = None
                         answerString = None
-        if msgContent[0:14] == "!battlefactory":
-            # Get the two participants
-            challenger = message.author
-            challenged = message.mentions[0]
-            battle = None
+        
+            # Battle factory code
+            if msgContent[0:14] == "!battlefactory":
+                print(f"Battle factory requested : {message.content}")
+                # Get the two participants
+                challenger = message.author
+                challenged = message.mentions[0]
+                battle = None
 
-            # Check for filters
-            if msgContent[14:15] == 'f':
-                filters = msgContent[15:].replace(' ', '').split(',')
-                leagueFilter = filters[0].upper()
-                formatFilter = int(filters[1])  # 0=all, 1=standard only, 2=special only
+                # Check for filters
+                if msgContent[14:15] == 'f':
+                    print(f"Battle factory filter")
+                    filters = msgContent[15:].replace(' ', '').split(',')
+                    leagueFilter = filters[0].upper()
+                    formatFilter = int(filters[1])  # 0=all, 1=standard only, 2=special only
 
-                # Filter possible battles
-                validBattles = []
-                for battle in bf.BATTLEFACTORY_DATA:
-                    league_name = bf.LEAGUE_INDEX.get(battle[0], "").upper()
-                    isStandard = battle[1]
+                    # Filter possible battles
+                    validBattles = []
+                    for battle in bf.BATTLEFACTORY_DATA:
+                        league_name = bf.LEAGUE_INDEX.get(battle[0], "").upper()
+                        isStandard = battle[1]
 
-                    # League must match
-                    if league_name != leagueFilter:
-                        continue
+                        # League must match
+                        if league_name != leagueFilter:
+                            continue
 
-                    # Format filter
-                    if formatFilter == 1 and not isStandard:
-                        continue
-                    elif formatFilter == 2 and isStandard:
-                        continue
+                        # Format filter
+                        if formatFilter == 1 and not isStandard:
+                            continue
+                        elif formatFilter == 2 and isStandard:
+                            continue
 
-                    validBattles.append(battle)
+                        validBattles.append(battle)
 
-                # Pick random from filtered
-                if validBattles:
-                    battle = random.choice(validBattles)
+                    # Pick random from filtered
+                    if validBattles:
+                        battle = random.choice(validBattles)
+                    else:
+                        # No matches found
+                        print("No battles matched your filters.")
                 else:
-                    # No matches found
-                    print("No battles matched your filters.")
-            elif msgContent[14:15] == ' ':
-                battle = random.choice(bf.BATTLEFACTORY_DATA)
+                    print(f"Battle factory standard")
+                    battle = random.choice(bf.BATTLEFACTORY_DATA)
 
-            if battle:
-                coin = random.randint(0,1)
+                if battle:
+                    coin = random.randint(0,1)
 
-                # Randomize order
-                if coin:
-                    home = battle[2]
-                    away = battle[3]
-                else:
-                    away = battle[2]
-                    home = battle[3]
+                    # Randomize order
+                    if coin:
+                        home = battle[4]
+                        away = battle[5]
+                    else:
+                        away = battle[4]
+                        home = battle[5]
+                    
+                    msg = (
+                        f"\n**WELCOME TO THE BATTLE FACTORY**\n\n"
+                        f"Fight for my amusement, *pigs*\n"
+                        f"**{bf.LEAGUE_INDEX.get(battle[0])} {battle[2]}**: *{home[0]}* vs *{away[0]}*\n\n"
+                    )
+
+                    teamHome = f"You will be playing as {home[0]}.\nYour captains are: {home[2]}\n{home[1]}\n"
+                    teamAway = f"You will be playing as {away[0]}.\nYour captains are: {away[2]}\n{away[1]}\n"
+
+                    # DM both users
+                    try:
+                        await challenger.send(f"Hey, {challenger.name}! You’re challenging {challenged.name}!\n\n{msg}{teamHome}\n")
+                    except discord.Forbidden:
+                        await message.channel.send(f"⚠️ I couldn’t fucking DM {challenger.mention}. Please enable DMs or shut the hell up!")
+
+                    try:
+                        await challenged.send(f"Hey, {challenged.name}! {challenger.name} has challenged you!\n\n{msg}{teamAway}\n")
+                    except discord.Forbidden:
+                        await message.channel.send(f"⚠️ I couldn’t fucking DM {challenger.mention}. Please enable DMs or shut the hell up!")
+
+                    # Optionally confirm in the channel
+                    await message.channel.send(f"Sent Battle Factory instructions to {challenger.mention} and {challenged.mention}!\n**{bf.LEAGUE_INDEX.get(battle[0])} {battle[2]}, {battle[3]}**: *{home[0]}* vs *{away[0]}*. BL do not HF!")
                 
-                msg = (
-                    f"\n**WELCOME TO THE BATTLE FACTORY**\n\n"
-                    f"Fight for my amusement, *pigs*\n"
-                    f"**{home[2]}**: *{home[3]}* vs *{away[3]}*\n\n"
-                )
-
-                teamHome = f"You will be playing as {home[3]}.\nYour captains are: {home[5]}\n{home[4]}\n"
-                teamAway = f"You will be playing as {away[3]}.\nYour captains are: {away[5]}\n{away[4]}\n"
-
-                # DM both users
-                try:
-                    await challenger.send(f"Hey, {challenger.name}! You’re challenging {challenged.name}!\n\n{msg}{teamHome}")
-                except discord.Forbidden:
-                    await message.channel.send(f"⚠️ I couldn’t fucking DM {challenger.mention}. Please enable DMs or shut the hell up!")
-
-                try:
-                    await challenged.send(f"Hey, {challenged.name}! {challenger.name} has challenged you!\n\n{msg}{teamAway}")
-                except discord.Forbidden:
-                    await message.channel.send(f"⚠️ I couldn’t fucking DM {challenger.mention}. Please enable DMs or shut the hell up!")
-
-                # Optionally confirm in the channel
-                await message.channel.send(f"Sent Battle Factory instructions to {challenger.mention} and {challenged.mention}!\n**{home[0]}**: *{home[1]}* vs *{away[1]}*. BL do not HF!")
+                else:
+                    await message.channel.send(f"Something may have gone wrong... Did you make a typo?")
             
-            else:
-                await message.channel.send(f"Something may have gone wrong... Did you make a typo?")
+            # Kill switch
+            if msgContent == "!kill Choc" and message.author.id == DEV:
+                print(f"Killing Choc-bot. Requested by {message.author.id}")
+                await message.channel.send("WHAT?! NOOOOOOO!!!")
+                
+                exit()
         
-        # Kill switch
-        if msgContent == "!kill Choc" and message.author.username == DEV:
-            await message.channel.send("WHAT?! NOOOOOOO!!!")
-            print(f"Killing Choc-bot. Requested by {message.author.username}")
-            exit()
-        
-
     except:
         print("Error : Index out of range for some reason: ", msgContent)
 
